7a1ad803c3164d8afff38eff7759e2be08e441a9
cleonissouza <cleonis-cls@outlook.com>
2025-11-10 18:18:56 +0000

    creating movie session

--- app/src/main/java/com/devspacecinenow/MainActivity.kt
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -14,6 +17,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,8 +29,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.devspacecinenow.ui.theme.CineNowTheme
 import retrofit2.Call
@@ -42,14 +50,14 @@ class MainActivity : ComponentActivity() {
                 val apiService = RetrofitClient.retrofitInstance.create(ApiService::class.java)
                 val callNowPlaying = apiService.getNowPlayingMovies()
 
-                callNowPlaying.enqueue(object  : Callback<MovieResponse>{
+                callNowPlaying.enqueue(object : Callback<MovieResponse> {
                     override fun onResponse(
                         call: Call<MovieResponse?>,
                         response: Response<MovieResponse?>
                     ) {
-                        if (response.isSuccessful){
+                        if (response.isSuccessful) {
                             val movies = response.body()?.results
-                            if (movies != null){
+                            if (movies != null) {
                                 nowPlayingMovies = movies
                             }
                         } else {
@@ -71,14 +79,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MovieList(
-                        nowPlayingMovies
-                    ){ movieClicked ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(8.dp),
+                            fontSize = 40.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            text = "CineNow",
+                        )
 
+                        MovieSession(
+                            label = "Now Playing",
+                            movieList = nowPlayingMovies,
+                            onClick = { movieClicked ->
+
+                            }
+                        )
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MovieSession(
+    label: String,
+    movieList: List<MovieDto>,
+    onClick: (MovieDto) -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = label,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        MovieList(movieList = movieList, onClick = onClick)
     }
 }
 
@@ -104,7 +147,9 @@ fun MovieItem(
 ) {
 
     Column(
-        modifier = Modifier.clickable{
+        modifier = Modifier
+            .width(IntrinsicSize.Min)
+            .clickable{
             onClick.invoke(movieDto)
         }
     ) {
@@ -116,6 +161,18 @@ fun MovieItem(
             contentScale = ContentScale.Crop,
             model = movieDto.posterFullPath,
             contentDescription = "${movieDto.title} Poster image"
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.SemiBold,
+            text = movieDto.title
+        )
+        Text(
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            text = movieDto.overview
         )
     }
 }
